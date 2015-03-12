a771a66d51a31b82a307119469f3375a5bc2bea2
Øystein Rindal <oystein_rindal@hotmail.com>
2015-03-12 12:56:52 +0100

    progress!!!

--- Processing.java
+++ Processing.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Processing extends PApplet{
-
+boolean state = true;
+float D = 2;
+int steps = 10000;
     ArrayList<Particle> particles;
     public void setup(){
-        size(500,500);
+        size(1920,1080);
         particles = new ArrayList<Particle>();
         for(int i=0; i<10000 ; i++){
             particles.add(new Particle(width/2,height/2));
@@ -20,10 +22,19 @@ public class Processing extends PApplet{
     }
 
     public void draw(){
-        background(0);
-        for(Particle p : particles){
-            p.display();
-            p.move();
+        if(state && steps > 0) {
+
+
+            background(0);
+            for(Particle p : particles){
+                p.display();
+                state = p.move();
+                if(!state) {
+                    break;
+                }
+
+            }
+            steps--;
         }
     }
 
@@ -50,17 +61,21 @@ public class Processing extends PApplet{
         }
         void display(){
             rectMode(CENTER);
-            stroke(0);
-            fill(255,0,0);
-            rect(x,y,10,10);
+            stroke(255,0,0);
+            fill(255,0 ,0);
+            rect(x,y,2,2);
         }
-        void move(){
-            
+        boolean move(){
             
             int ind = randomGenerator.nextInt(9);
-            x=x+dir[ind][0];
-            y=y+dir[ind][1];
+            x=x+dir[ind][0]*D;
+            y=y+dir[ind][1]*D;
+            if(x > width || y > height || x < 0 || y < 0) {
+                return false;
+            }
+            return true;
         }
+        
     }
 
     public static void main(String[] args) {
